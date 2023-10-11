5fef9f9dab78beffee8b1842cde09cbd5a395c51
Kyle M <kylem61@googlemail.com>
2023-10-11 11:50:12 +0100

    Adds logic to check if letter is valid. TODO: Fix current bug when checking if guess is in the random word.

--- milestone_2.py
+++ milestone_2.py
@@ -8,9 +8,9 @@ def random_choice(choice):
 
 random_choice(word_list)
 
-guess = input("Enter a single letter...")
+letter_guess = input("Enter a single letter...")
 
-if (len(guess) == 1) and (guess.isalpha() == True):
+if (len(letter_guess) == 1) and (letter_guess.isalpha() == True):
     print("Good guess!")
 else:
     print("Oops! That is not a valid input!")
---
+++ milestone_3.py
@@ -0,0 +1,16 @@
+from milestone_2 import word
+
+while True:
+    guess = input("Guess a letter.")
+
+    if guess.isalpha() == True:
+        break
+    else:
+        print("Invalid letter. Please enter a single alphabetical character.")
+
+        
+if guess in word:
+        print(f"Good guess! {guess} is in the word")
+    
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
