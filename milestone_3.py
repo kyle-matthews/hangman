57be2d9aaaa056d76cadd542a76034242327af04
Kyle M <kylem61@googlemail.com>
2023-10-11 12:13:58 +0100

    Condenses all coding into one file for enhanced readability

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,19 @@
-import milestone_2
+import random
+word_list = ["melon", "grape", "banana", "apple", "pineapple"]
+print(word_list)
+
+def random_choice(choice):
+    word = random.choice(word_list)
+    return word
+
+random_choice(word_list)
+
+letter_guess = input("Enter a single letter...")
+
+if (len(letter_guess) == 1) and (letter_guess.isalpha() == True):
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input!")
 
 while True:
     guess = input("Guess a letter.")
