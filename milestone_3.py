82412fc4bb41dea6db99494434424e4eb7e3aea5
Kyle M <kylem61@googlemail.com>
2023-10-11 13:18:32 +0100

    Tidies up code for readability.

--- milestone_3.py
+++ milestone_3.py
@@ -4,28 +4,20 @@ print(word_list)
 
 def random_choice(list):
     word = random.choice(word_list) 
-    print("word")
+    return word
 
 word = random_choice(word_list)
-
-letter_guess = input("Enter a single letter...")
-
-if (len(letter_guess) == 1) and (letter_guess.isalpha() == True):
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input!")
 
 while True:
     guess = input("Guess a letter.")
 
-    if guess.isalpha() == True:
-        break
+    if (len(guess) == 1) and (guess.isalpha() == True):
+        if guess in word:
+            print(f"Good guess! {guess} is in the word")
+    
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+        
     else:
         print("Invalid letter. Please enter a single alphabetical character.")
 
-
-if guess in word:
-        print(f"Good guess! {guess} is in the word")
-    
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
