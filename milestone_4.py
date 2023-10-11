a01c09cbe171825e3ed0584b20cf6103c558694d
Kyle M <kylem61@googlemail.com>
2023-10-11 14:14:59 +0100

    Adds a new branch for the game class feature

---
+++ milestone_4.py
@@ -0,0 +1,30 @@
+import random
+word_list = ["melon", "grape", "banana", "apple", "pineapple"]
+print(word_list)
+
+def random_choice(list):
+    word = random.choice(word_list) 
+    return word
+
+word = random_choice(word_list)
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter.").lower()
+        
+        if (len(guess) == 1) and (guess.isalpha() == True):
+            if guess in word:
+                check_guess(guess)
+            else:
+                print("Invalid letter. Please enter a single alphabetical character.")
+
+
+
+ask_for_input()
