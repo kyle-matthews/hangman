7406278cb3f0b74736392b4dfbda54ff5e1decf8
Kyle M <kylem61@googlemail.com>
2023-10-12 17:54:44 +0100

    Adds functionality to update the word_guessed method and remove lives if incorrect.

--- milestone_4.py
+++ milestone_4.py
@@ -1,17 +1,25 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list, num_lives = 5):
-        self.word = random.choice(word_list)
+    def __init__(self, num_lives = 5):
+        self.word_list = ["melon", "grape", "banana", "apple", "pineapple"]
+        self.word = random.choice(self.word_list)
         self.word_guessed = ['_' for character in self.word]
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
+
         self.list_of_guesses = []
 
     def check_guess(self, guess):
         self.guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word!")
+            for character in guess:
+                if character == guess:
+                    for i in range(len(self.word)):
+                        if self.word[i] == guess:
+                            self.word_guessed[i] = guess    
+            self.num_letters -= 1
     
     def ask_for_input(self):
         while True:
