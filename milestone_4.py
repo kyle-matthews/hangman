16177297bdbf2c850f401990498694f9b06659c5
Kyle M <kylem61@googlemail.com>
2023-10-12 16:49:21 +0100

    Removes duplicate code for enhanced readability.

--- milestone_4.py
+++ milestone_4.py
@@ -1,19 +1,5 @@
 import random
 
-def ask_for_input():
-    while True:
-        guess = input("Guess a letter.").lower()
-        
-        if (len(guess) == 1) and (guess.isalpha() == True):
-            if guess in word:
-                check_guess(guess)
-            else:
-                print("Invalid letter. Please enter a single alphabetical character.")
-
-
-
-ask_for_input()
-
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
@@ -28,10 +14,15 @@ class Hangman:
         if guess in self.word:
             print(f"Good guess! {guess} is in the word!")
     
-    def ask_for_input():
+    def ask_for_input(self):
         while True:
             guess = input("Guess a letter...")
             if guess.isalpha() != True:
-                print("Invalid letter. Please, enter a single aplphabetical character.")
-            elif guess in self.list_of_guesses:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in Hangman.list_of_guesses:
                 print("You already tried that letter!")
+            else:
+                Hangman.check_guess(guess)
+                Hangman.list_of_guesses.append(guess)
+
+Hangman.ask_for_input()
